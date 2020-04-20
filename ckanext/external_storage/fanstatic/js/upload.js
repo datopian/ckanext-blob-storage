b876b71b289bf239d87d15d712f93b6782956df1
Shahar Evron <shahar.evron@gmail.com>
2020-04-20 14:07:23 +0300

    Use jQuery Defer to synchronize CKAN API call to get token

--- ckanext/external_storage/fanstatic/js/upload.js
+++ ckanext/external_storage/fanstatic/js/upload.js
@@ -12,6 +12,7 @@ ckan.module('external-storage-upload', function($) {
 
         initialize: function () {
             console.log('Initializing external-storage-upload CKAN JS module');
+            $.proxyAll(this, /_on/);
 
             // this._url = $('#field-image-url');
             this._form = this.$('form');
@@ -22,32 +23,58 @@ ckan.module('external-storage-upload', function($) {
             this._save.on('click', this._onFormSubmit);
         },
 
-        _onFormSubmit: function(event, pass) {
+        _onFormSubmit: function(event) {
             // Check if we have anything to upload
-            // if (pass || ! window.FileList || ! this._file || ! this._file.val()) {
-            //     return;
-            // }
-            event.preventDefault();
-            console.log($(this._form));
+            if (! window.FileList || ! this._file || ! this._file.val()) {
+                return;
+            }
 
+            event.preventDefault();
             var prefix = this.options.storagePrefix.split('/');
-            console.log(this._getAuthzToken());
+            var scopes = [this.options.authzScope];
+            var serverUrl = this.options.serverUrl;
+
+            this._getAuthzToken(scopes).then(function(token) {
+                console.log(token);
+
+                const test = new ckanUploader.DataHub(
+                    token,
+                    prefix[0],
+                    prefix[1],
+                    serverUrl
+                );
+
+                const resources = {
+                  basePath: 'test/fixtures',
+                  path: 'sample.csv',
+                };
+
+                return test.push(resources);
+            }).then(function(result) {
+                console.log(result);
+            });
         },
 
-        _getAuthzToken: function () {
+        _getAuthzToken: function (scopes) {
+            var dfd = $.Deferred();
+
             this.sandbox.client.call(
                 'POST',
                 'authz_authorize',
-                {scopes: [this.options.authzScope]},
+                {scopes: scopes},
 
                 function (data) {
-                    console.log(data)
+                    // TODO: Check that we got the scopes we need
+                    dfd.resolve(data.result.token);
                 },
 
                 function (error) {
                     console.log(error);
+                    dfd.reject(error);
                 }
             );
+
+            return dfd.promise();
         }
     };
 });
--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,15 @@ module.exports = {
   mode: 'production',
   devtool: "source-map",
   context: path.resolve(__dirname),
-	entry: {
+  entry: {
     "index": `./node_modules/ckan3-js-sdk/lib/index.js`,
   },
-	output: {
+  output: {
     path: path.resolve(__dirname, assetPath, 'dist'),
     filename: '[name].js',
     libraryTarget: 'var',
     library: 'ckanUploader',// The variable name to access the library
-	},
+  },
   module: {
     rules: [
       {
