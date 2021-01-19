9c8440bd2d1f86190f0edce7aae760d00341c39f
Shahar Evron <shahar.evron@datopian.com>
2021-01-19 18:34:45 +0200

    isort fix - use relative import

--- ckanext/blob_storage/authz.py
+++ ckanext/blob_storage/authz.py
@@ -8,7 +8,7 @@ from ckanext.authz_service.authz_binding import resource as resource_authz
 from ckanext.authz_service.authz_binding.common import get_user_context
 from ckanext.authz_service.authzzie import Scope
 
-from ckanext.blob_storage import helpers
+from . import helpers
 
 log = logging.getLogger(__name__)
 
