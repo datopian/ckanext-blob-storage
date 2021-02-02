f084d5f04eeff62fda0bbc3319bbfff60ed2280a
pdelboca <patriciodelboca@gmail.com>
2021-02-02 11:17:53 -0300

    Improve testing coverage

--- ckanext/external_storage/tests/test_validators.py
+++ ckanext/external_storage/tests/test_validators.py
@@ -1,3 +1,6 @@
+import pytest
+from ckan.plugins.toolkit import Invalid
+
 from ckanext.external_storage import validators
 
 
@@ -10,6 +13,13 @@ def test_upload_has_sha256():
     }
     validators.upload_has_sha256(key, flattened_data, {}, {})
 
+    with pytest.raises(Invalid):
+        flattened_data = {
+            (u'resources', 0, u'url_type'): u'upload',
+            (u'resources', 0, u'url'): u'/my/file.csv',
+        }
+        validators.upload_has_sha256(key, flattened_data, {}, {})
+
 
 def test_upload_has_size():
     key = ('resources', 0, 'url_type')
@@ -19,6 +29,13 @@ def test_upload_has_size():
         (u'resources', 0, u'size'): 123456
     }
     validators.upload_has_size(key, flattened_data, {}, {})
+
+    with pytest.raises(Invalid):
+        flattened_data = {
+            (u'resources', 0, u'url_type'): u'upload',
+            (u'resources', 0, u'url'): u'/my/file.csv',
+        }
+        validators.upload_has_size(key, flattened_data, {}, {})
 
 
 def test_upload_has_lfs_prefix():
@@ -30,9 +47,19 @@ def test_upload_has_lfs_prefix():
     }
     validators.upload_has_lfs_prefix(key, flattened_data, {}, {})
 
+    with pytest.raises(Invalid):
+        flattened_data = {
+            (u'resources', 0, u'url_type'): u'upload',
+            (u'resources', 0, u'url'): u'/my/file.csv'
+        }
+        validators.upload_has_lfs_prefix(key, flattened_data, {}, {})
+
 
 def test_valid_sha256():
     validators.valid_sha256('cc71500070cf26cd6e8eab7c9eec3a937be957d144f445ad24003157e2bd0919')
+
+    with pytest.raises(Invalid):
+        validators.valid_sha256('wrong_sha256')
 
 
 def test_sha256_doesnt_raise_if_not_upload():
